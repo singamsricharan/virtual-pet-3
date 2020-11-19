d12d542a7e9809a179975bd1314e0eccb76d3c31
sricharan <singam.sricharan1603@gmail.com>
2020-11-19 07:56:03 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 //Create variables here
 var dog, happyDog, database, foodS, foodStock,database;
+var fedTime,lastfed;
+var foodObj;
 function preload(){
   //load images here
   dogimg=loadImage("images/dogImg.png")
@@ -11,24 +13,48 @@ function setup(){
   dog=createSprite(250,250);
   dog.addImage(dogimg);
   dog.scale=0.2;
+  foodObj=new Food();
   database = firebase.database();
   foodStock=database.ref('Food');
   foodStock.on("value",readstock);
+  feed=createButton("feed the dog");
+  feed.position(700,95);
+  feed.mousePressed(feedDog);
+
+  addFood=createButton("Add Food");
+  addFood.position(800,95);
+  addFood.mousePressed(addFoods);
 }
 
 
 function draw(){  
   background(46,139,87);
-  if(keyWentDown(UP_ARROW)){
-    writeStock(foodS);
-    dog.addImage(dog2img);
-  }
+  fedTime=database.ref('FeedTime');
+  fedTime.on("value",function(data){
+    lastfed=data.val();
+  })
+  foodObj.display();
   drawSprites();
   //add styles here
   textSize(30);
   fill("black");
   stroke("black");
   text("FoodRemaining:"+foodS,100,100)
+}
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
+function feedDog(){
+  dog.addImage(dog2img);
+
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
+  database.ref('/').update({
+    food:foodObj.getFoodStock(),
+    fedTime:hour()
+  })
 }
 //Fuction to read values from db
 function readstock(data){
